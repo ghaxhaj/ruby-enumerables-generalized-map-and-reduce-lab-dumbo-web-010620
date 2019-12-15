b70faabd0d1b2606e1fe21ef93a7dcc27658b3a5
wise-integer-6834 <ghaxhaj@fordham.edu>
2019-12-15 21:11:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,10 @@
-map to negativize[1,2,3] do |num|
-  num * -1
+def map_to_negativize(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new = (source_array[i] * -1)
+    i += 1
+    yield new
+  end
+  return new
 end
